6a133a9bc4a6d29fd09057b853ed62bb826f0446
Em Jordan <sylvrs@proton.me>
2022-01-28 23:42:22 -0600

    Set JVM target to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 kotlin {
     jvm {
         compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.jvmTarget = "1.11"
         }
         testRuns["test"].executionTask.configure {
             useJUnit()
